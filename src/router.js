4da9ff9ff273ebb1853cb0763afbe499e00b1eed
MR.R <razor.wu1994@gmail.com>
2019-06-26 17:07:09 -0400

    change

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: "hash",
-  base: "/riddles/",
+  base: "/Browser-Riddles/",
   routes: [
     {
       path: "/",
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/riddles/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/Browser-Riddles/" : "/"
 };
