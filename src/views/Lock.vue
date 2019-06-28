1d99be7de6aacb964761ee5452a846e4fd1f6417
MR.R <razor.wu1994@gmail.com>
2019-06-28 07:43:46 -0400

    v-if

--- src/views/Lock.vue
+++ src/views/Lock.vue
@@ -3,14 +3,27 @@
     <h1>Let me out!!!</h1>
     <h2>Who keeps me here</h2>
     <div class="side">
-      <button @click="restart()">
-        <h1>(Not recommend)Restart game(get out!)</h1>
+      <button @click="restart()" v-if="showRestart">
+        <h1>(You've been really struggle)Restart the game to get out!</h1>
       </button>
     </div>
   </div>
 </template>
 <script>
 export default {
+  beforeCreate: function() {
+    let refreshTime = parseInt(localStorage.getItem("REFRESH_TIME"), 10);
+    if (!isNaN(refreshTime)) {
+      localStorage.setItem("REFRESH_TIME", refreshTime + 1);
+    } else {
+      localStorage.setItem("REFRESH_TIME", 1);
+    }
+  },
+  data: function() {
+    return {
+      showRestart: parseInt(localStorage.getItem("REFRESH_TIME"), 10) > 5
+    };
+  },
   mounted: function() {
     localStorage.setItem("IWANNAADIFFENTVALUE", "yes");
   },
