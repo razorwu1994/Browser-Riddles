d81e856889501d1ee397aefdfb2a661f89a0d3e1
MR.R <razor.wu1994@gmail.com>
2019-07-23 20:30:39 -0400

    hide todo

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackCdnPlugin = require("webpack-cdn-plugin");
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new WebpackCdnPlugin({
+        modules: [
+          {
+            name: "vue",
+            var: "Vue",
+            path: "dist/vue.runtime.min.js"
+          },
+          {
+            name: "vue-router",
+            var: "VueRouter",
+            path: "dist/vue-router.min.js"
+          },
+          {
+            name: "vuex",
+            var: "Vuex",
+            path: "dist/vuex.min.js"
+          }
+        ],
+        publicPath: "/node_modules"
+      })
+    ]
+  },
   publicPath: process.env.NODE_ENV === "production" ? "/Browser-Riddles/" : "/"
 };
