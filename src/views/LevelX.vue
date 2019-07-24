8bb62e2a9f762bf07c77f8f7774f14bc9fc39736
MR.R <razor.wu1994@gmail.com>
2019-07-24 00:09:52 -0400

    10 th

--- src/views/LevelX.vue
+++ src/views/LevelX.vue
@@ -104,13 +104,12 @@ export default {
     return {
       rowMessage: rowMessage,
       codeMessage: [
-        "这一关做在第一阶段",
-        "因为也是属于考察浏览器其中一个tab的",
-        "把反向解码代码整段（就是可以复制粘贴到console 运行起来的)",
-        "例子： const a =(数字)=>字母; 补充上使用方法 a（10101010）= S 这个字母",
-        "密钥就是上面的按钮里的数字，每次都随机的数字 范围在0～25之间",
-        "加密过程就是把26个字母从1到26编排，然后乘以8+这个随机的密钥",
-        "大概26*8+25 = 233，也是为了防止转换2进制溢出8位数"
+        `const gift = Your_GIFT`,
+        "const getKey=(data)=>{",
+        "return (parseInt(data,2)-gift)/8}",
+        `const charDictionary = JSON.parse('{"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}')`,
+        "const finalDecipher =(key)=>{const k = `${key}` ; return Object.keys(charDictionary)[getKey(k)-1]}",
+        "finalDecipher(number_on_the_page_one_by_one)"
       ],
       secretKey: secretKey,
       showKey: false
@@ -131,11 +130,14 @@ export default {
 .way {
   text-align: left;
   margin-left: 15px;
+  width: 600px;
+  user-select: text;
 }
 .code {
   font-size: 20px;
   font-weight: bold;
   font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
     "Lucida Sans", Arial, sans-serif;
+  word-break: break-word;
 }
 </style>
