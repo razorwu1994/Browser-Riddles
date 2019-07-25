d2c946909b6fa91d2abd3b4d1f392cd70c507e0b
MR.R <razor.wu1994@gmail.com>
2019-07-25 00:09:59 -0400

    fix

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -2,12 +2,66 @@
   <div class="intro">
     <h1>Welcome to Browser Riddles</h1>
     <p>A nonstop journey of learning web development</p>
-    <p>Bon voyage!</p>
-    <button @click="function(){$router.replace('/competitor')}" class="start">Start</button>
+    <button
+      @click="function(){$router.replace('/competitor')}"
+      class="start"
+      v-if="computedDisplayAnimation"
+    >Bon voyage!</button>
   </div>
 </template>
 <script>
+export default {
+  data: function() {
+    return {
+      open: false,
+      displayAnimation: false
+    };
+  },
+  created() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  },
+  destroyed() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+  },
+  mounted() {
+    let start = new Date(),
+      end = start;
+    setInterval(() => {
+      end = new Date();
+      if (!this.open) {
+        start = end;
+      } else if ((end - start) / 1000 > 2) {
+        setTimeout(() => {
+          this.displayAnimation = true;
+        }, 15000);
+      }
+    }, 500);
+  },
+  methods: {
+    handleMouseMove: function(evt, el) {
+      let [h, w] = [window.innerHeight, window.innerWidth];
+      let mouseX = parseInt(evt.clientX, 10);
+      let mouseY = parseInt(evt.clientY, 10);
+      if (
+        mouseY > (h * 9) / 10 &&
+        mouseX < w / 10 + h / 10 &&
+        mouseX > w / 10 - h / 10
+      ) {
+        this.open = true;
+      } else {
+        this.open = false;
+      }
+      return;
+    }
+  },
+  computed: {
+    computedDisplayAnimation() {
+      return this.displayAnimation;
+    }
+  }
+};
 </script>
+
 <style scoped>
 .start {
   width: 400px;
